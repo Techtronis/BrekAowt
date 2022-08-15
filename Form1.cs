0bde0892370e59db210f0298d7a25ff848f59cbd
Techtronis <alexisjost20@gmail.com>
2022-08-15 15:58:00 +0200

    .

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Drawing;
+using System.Formats.Asn1;
+
 namespace BrékAowt
 {
+
     public partial class Form1 : Form
     {
+
+        class Cords
+        {
+            public Cords(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+        }
+
         private int ballMoveX = 0;
         private int ballMoveY = 3;
         private int ballX;
@@ -13,7 +34,9 @@ namespace BrékAowt
         private string popUpText = "Press 'Space' to Start";
         int pX = 369;
         int pY = 1100;
-        public Form1()
+        Cords[] _cords;
+
+        public void Form1_Load(object sender, EventArgs e)
         {
             InitializeComponent();
 
@@ -25,16 +48,23 @@ namespace BrékAowt
             timer1.Start();
 
             Cursor.Hide();
+
+            List<Cords> cords = new();
+
+            foreach (var item in Controls)
+            {
+                if (item is PictureBox pb)
+                {
+                    cords.Add(new(pb.Location.X, pb.Location.Y));
+                    
+                }
+            }
+
+            _cords = cords.ToArray();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
             if (e.KeyCode == Keys.Right) pX += 25;
             else if (e.KeyCode == Keys.Left) pX -= 25;
             else if (gameOver && e.KeyCode == Keys.Space)
@@ -42,11 +72,13 @@ namespace BrékAowt
                 gameOver = false;
                 boxCount = 36;
                 lives = 3;
+                Reset();
             }
             paddle.Location = new Point(pX, paddle.Location.Y);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
+
         {
             Random randX = new Random();
             ballX = ball.Location.X;
@@ -86,7 +118,7 @@ namespace BrékAowt
                             Score++;
                             this.Controls.Remove(x);
                             ballMoveY = -ballMoveY;
-                            
+
                         }
                     }
                     else if (x is PictureBox && x.Tag == "pictureBox" && gameOver == true)
@@ -99,6 +131,7 @@ namespace BrékAowt
                 {
                     boxCount = -1;
                     gameOver = true;
+
                     ballX = 476;
                     ballY = 751;
                     ballMoveX = 0;
@@ -118,16 +151,43 @@ namespace BrékAowt
                     gameOver = true;
                     boxCount = -2;
                 }
-            } else if (gameOver && boxCount == -1)
+            }
+            else if (gameOver && boxCount == -1)
             {
                 popUpText = "You won!\nPress 'Space' to start again";
                 popUp.Text = popUpText;
-            } else if (gameOver && boxCount == -2)
+            }
+            else if (gameOver && boxCount == -2)
             {
                 popUpText = "You lost!\nPress 'Space' to try again";
                 popUp.Text = popUpText;
+                Reset();
             }
-            
+        }
+        private void Reset()
+        {
+            for (int i = Controls.Count - 1; i >= 0; i--)
+            {
+                if (Controls[i].Tag == "pictureBox" && Controls[i] is PictureBox pb)
+                {
+                    pb.Dispose();
+                    Controls.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < _cords.Length; ++i)
+            {
+                PictureBox pb = new()
+                {
+                    Name = $"pictureBox{i}",
+                    Tag = "pictureBox",
+                    Parent = this,
+                    BackColor = Color.White,
+                    Location = new Point(_cords[i].X, _cords[i].Y),
+                    Size = new Size(10, 10)
+                };
+                Controls.Add(pb);
+            }
         }
     }
 }
